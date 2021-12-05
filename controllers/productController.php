7b401727ea64012c3d672679c471a3180ed2f8c9
fernandomduarte <fernando.duarte05@gmail.com>
2021-12-05 11:35:08 -0300

    segurança no edit e sell aplicada

--- controllers/productController.php
+++ controllers/productController.php
@@ -49,79 +49,93 @@ class productController extends Controller {
 	}
 
 	public function edit($id) {
-		$data = array();
-		$image = array();
-			
-		$p = new Products();
-		$f = new FiltersHelper();
+		if ($id) {
+			$data = array();
+			$image = array();
+				
+			$p = new Products();
+			$f = new FiltersHelper();
 
-		if (isset($_FILES['image']) && !empty($_FILES['image']['tmp_name'])) {
-			$image = $_FILES['image'];
-			$image = $this->saveImage($image, $id);
+			if (isset($_FILES['image']) && !empty($_FILES['image']['tmp_name'])) {
+				$image = $_FILES['image'];
+				$image = $this->saveImage($image, $id);
 
-			if ($image) {
-				$p->editImage($image, $id);
+				if ($image) {
+					$p->editImage($image, $id);
 
-				header("Location: ".BASE_URL);
-				exit;
+					header("Location: ".BASE_URL);
+					exit;
+				}
+
 			}
 
-		}
+			if (!empty($_POST['name'])) {
+				$name = ucfirst(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
+				$quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
+				$min_quantity = filter_input(INPUT_POST, 'min_quantity', FILTER_VALIDATE_INT);
+				$price = $f->filter_float('price');
 
-		if (!empty($_POST['name'])) {
-			$name = ucfirst(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
-			$quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
-			$min_quantity = filter_input(INPUT_POST, 'min_quantity', FILTER_VALIDATE_INT);
-			$price = $f->filter_float('price');
+				if ($name && $quantity && $min_quantity && $price) {
+					$p->editProduct($name, $quantity, $min_quantity, $price, $id);
 
-			if ($name && $quantity && $min_quantity && $price) {
-				$p->editProduct($name, $quantity, $min_quantity, $price, $id);
-
-				header("Location: ".BASE_URL);
+					header("Location: ".BASE_URL);
+					exit;
+				} 
+				header("Location: ".BASE_URL."edit");
 				exit;
-			} 
-			header("Location: ".BASE_URL."edit");
+					
+			}
+			$data['info'] = $p->getOne($id);
+			
+			$this->loadTemplate('edit', $data);
+
+		} else {
+			header("Location: ".BASE_URL);
 			exit;
-				
 		}
-		$data['info'] = $p->getOne($id);
-		
-		$this->loadTemplate('edit', $data);
+
 	}
 
 	public function sell($id) {
-		$data = array();
+		if ($id) {
+			$data = array();
 		
-		$p = new Products();
-		$s = new Sales();
-		$f = new FiltersHelper();
+			$p = new Products();
+			$s = new Sales();
+			$f = new FiltersHelper();
 
-		$data['info'] = $p->getOne($id);
-		$new_qtd = $data['info']['quantity'];
-		$stock = $data['info']['quantity'];
+			$data['info'] = $p->getOne($id);
+			$new_qtd = $data['info']['quantity'];
+			$stock = $data['info']['quantity'];
 
-		if (!empty($_POST['s_price']) && $_POST['s_quantity'] > 0 && $_POST['s_quantity'] <= $stock) {
-			$code = $_POST['code'];
-			$name = $_POST['name'];
-			$s_price = $f->filter_float('s_price');
-			$s_quantity = filter_input(INPUT_POST, 's_quantity', FILTER_VALIDATE_INT);
+			if (!empty($_POST['s_price']) && $_POST['s_quantity'] > 0 && $_POST['s_quantity'] <= $stock) {
+				$code = $_POST['code'];
+				$name = $_POST['name'];
+				$s_price = $f->filter_float('s_price');
+				$s_quantity = filter_input(INPUT_POST, 's_quantity', FILTER_VALIDATE_INT);
 
-			if ($code && $name && $s_price && $s_quantity) {
-				$s->sellProduct($code, $name, $s_price, $s_quantity);
+				if ($code && $name && $s_price && $s_quantity) {
+					$s->sellProduct($code, $name, $s_price, $s_quantity);
 
-				$new_qtd = $new_qtd - $s_quantity;
-				$p->updateQuantity($id, $new_qtd);
+					$new_qtd = $new_qtd - $s_quantity;
+					$p->updateQuantity($id, $new_qtd);
 
-				header("Location: ".BASE_URL."sales/historic");
-				exit;
+					header("Location: ".BASE_URL."sales/historic");
+					exit;
 
-			} else {
-				header("Location: ".BASE_URL."sell");
-				exit;
+				} else {
+					header("Location: ".BASE_URL."sell");
+					exit;
+				}
+
 			}
-
+			$this->loadTemplate('sell', $data);
+			
+		} else {
+			header("Location: ".BASE_URL);
+			exit;
 		}
-		$this->loadTemplate('sell', $data);
+		
 	}
 
 	private function saveImage($image, $id = null) {
